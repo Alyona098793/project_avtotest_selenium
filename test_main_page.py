3cc0bd4fc5c34761c0587f9fba133701f9269f07
Alyona Nagorna <aljona2002605@gmail.com>
2022-08-21 20:37:27 +0300

    Реализация проверки логина страници и формы регистрации

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
-from .pages.main_page import MainPage
 
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -9,7 +10,9 @@ def test_guest_can_go_to_login_page(browser):
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
